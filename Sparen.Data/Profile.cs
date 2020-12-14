24197bd43190ae5db0c13b2e23b49499a1ce891c
ajbain <ashleytiller@gmail.com>
2020-12-14 15:25:51 -0500

    Add project files.

---
+++ Sparen.Data/Profile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparen.Data
+{
+    public class Profile
+
+    {
+        [Key]
+        public int ProfileId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+
+        [Phone]
+        public string MobilePhone { get; set; }
+
+        //list of Checking Accounts
+        //List of Savings Accounts
+
+    }
+}
